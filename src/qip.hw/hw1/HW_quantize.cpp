e66331972600f27e38e171a7acdeea851aa274f6
Weifan Lin <wlin15@citymail.cuny.edu>
2016-09-21 12:54:29 -0400

    finish Quantize

--- src/qip.hw/hw1/HW_quantize.cpp
+++ src/qip.hw/hw1/HW_quantize.cpp
@@ -4,9 +4,29 @@
 // Quantize I1 to specified number of levels. Apply dither if flag is set.
 // Output is in I2.
 //
-// Written by: ADD YOUR NAMES HERE, 2016
+// Written by: Weifan Lin & Kaimin Jiang, 2016
 //
 void
 HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
 {
+	IP_copyImageHeader(I1, I2);
+	int w = I1->width ();
+	int h = I1->height();
+	int total = w * h;
+
+    int scale = MXGRAY / levels;
+    double bias = scale/2.0;
+
+	// init lookup table
+	int i, lut[MXGRAY];
+	for(i=0; i<MXGRAY; ++i)
+		lut[i] = CLIP(int(scale * (i/scale) + bias), 0, 255);
+
+	// evaluate output: each input pixel indexes into lut[] to eval output
+	int type;
+	ChannelPtr<uchar> p1, p2, endd;
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endd = p1 + total; p1<endd;) *p2++ = lut[*p1++];
+	}
 }
