5a68c3408c43285ed4c780ad384730b7574b0ef7
Weifan Lin <wlin15@citymail.cuny.edu>
2016-10-31 17:11:39 -0400

    finish sharpen

--- src/qip2.hw/hw2/HW_sharpen.cpp
+++ src/qip2.hw/hw2/HW_sharpen.cpp
@@ -9,4 +9,27 @@ extern void HW_blur1D(ChannelPtr<uchar>, int, int, int, ChannelPtr<uchar>);
 void
 HW_sharpen(ImagePtr I1, int size, double factor, ImagePtr I2)
 {
+    IP_copyImageHeader(I1, I2);
+    int w = I1->width();
+    int h = I1->height();
+    int total = w * h;
+    int type;
+    ChannelPtr<uchar> p1, p2, endd;
+
+    if(size == 1) {
+        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+            IP_getChannel(I2, ch, p2, type);
+            for(endd = p1 + total; p1<endd;) *p2++ = *p1++;
+        }
+    } else {
+        HW_blur(I1, size, size, I2);
+        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+            IP_getChannel(I2, ch, p2, type);
+            for(endd = p1 + total; p1<endd;) {
+                *p2 = CLIP(*p1 + (*p1 - *p2) * factor, 0, 255);
+                *p1++;
+                *p2++;
+            }
+        }
+    }
 }
